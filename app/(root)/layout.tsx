e37def1e2f91ec39053c32907fe6a3c192dd23f0
SNG66-ReadyChatAI <johny.gutierrez@readychatai.com>
2026-01-25 11:09:02 -0400

    left side bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react'
 import Navbar from '@/components/navigations/navbar'
 import LeftSideBar from '@/components/navigations/LeftSideBar'
+import RightSideBar from '@/components/navigations/RightSideBar'
 
 const RootLayout = ( {children}: {children: ReactNode} ) => {
   return (
@@ -12,6 +13,8 @@ const RootLayout = ( {children}: {children: ReactNode} ) => {
         <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb14 sm:px-14'>
           <div className='mx-auto w-full max-w-5xl'>{children}</div>
         </section>
+
+        <RightSideBar />
 
       </div>
       
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,16 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { techMap } from "../constants/techMap"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const getDeviconClassName = (techName: string) => {
+  const normalizedTechName = techName.replace(/[ .]/g, "").toLowerCase();  // This is just to clean up the techName, repalcing spaces and dots for empty string and lowecasing it
+
+
+  return techMap[normalizedTechName] 
+        ? `${techMap[normalizedTechName]} colored` 
+        : "devicon-devicon-plain";
 }
