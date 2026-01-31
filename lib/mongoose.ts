19e44791da2ff95bcb3cf227794bb41d4e6e1b12
SNG66-ReadyChatAI <johny.gutierrez@readychatai.com>
2026-01-31 19:04:57 -0400

    Setting up mongodb and mongoose

---
+++ lib/mongoose.ts
@@ -0,0 +1,59 @@
+import mongoose, { Mongoose } from "mongoose";
+
+
+// Define MongoDB URI
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+if(!MONGODB_URI) {
+    throw new Error("MONGODB_URI is not defined");
+}
+
+
+interface MongooseCache {
+    conn: Mongoose | null;
+    promise: Promise<Mongoose> | null;
+}
+
+
+declare global {
+    // Using "var" so we can have a global version of the mongoose variable
+    var mongoose: MongooseCache  // This is just for typescript
+}
+
+let cached = global.mongoose;
+
+if(!cached) {
+    // We're doing this becaise Next.jsw functions can be invoked multiple times, especially in development.
+    // So without caching, each invocation create a new database connection, leading to resource exhaustion and potential
+    // connection limits being reached
+    cached = global.mongoose = { conn: null, promise: null };
+}
+
+// Establishing the connnection
+const dbConnect = async (): Promise<Mongoose> => {   // Is going to return a Promise, so we define that type
+    if(cached.conn) {
+        return cached.conn; //Check if there's a cached connection and if so, return that one
+    }
+
+    // If not cache, then create a new PROMISE for connecting to the database
+    if(!cached.promise) {
+        cached.promise = mongoose.connect(MONGODB_URI, {
+            dbName: "DevflowCluster",
+        })
+        .then((result) => {
+            console.log("Conected to MongoDB");
+            return result;
+
+        }).catch((error) => {
+            console.error("Error connecting to MongoDB", error);
+            throw error;
+        });
+    }
+
+    // wait (resolve) for the promise to resolve
+    cached.conn = await cached.promise;
+
+    return cached.conn;
+};
+
+export default dbConnect;
